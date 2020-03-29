3fe673d01bea6d94f3fe8a98b6f50a89e5693084
Matt Dieball <2754460+madman22@users.noreply.github.com>
2020-03-29 12:16:06 -0400

    Add support for using an existing connection
    Create UseConnection and UseConnectionDbg for using an existing connection.  Also modifies the existing Connect functions to use these new functions to reduce code duplication.

--- ftp.go
+++ ftp.go
@@ -724,42 +724,38 @@ func (ftp *FTP) Login(username string, password string) (err error) {
 
 // Connect to server at addr (format "host:port"). debug is OFF
 func Connect(addr string) (*FTP, error) {
-	var err error
-	var conn net.Conn
-
-	if conn, err = net.Dial("tcp", addr); err != nil {
+	conn, err := net.Dial("tcp", addr)
+	if err != nil {
 		return nil, err
 	}
-
-	writer := bufio.NewWriter(conn)
-	reader := bufio.NewReader(conn)
-
-	//reader.ReadString('\n')
-	object := &FTP{conn: conn, addr: addr, reader: reader, writer: writer, debug: false}
-	object.receive()
-
-	return object, nil
+	return UseConnection(addr, conn)
 }
 
 // ConnectDbg to server at addr (format "host:port"). debug is ON
 func ConnectDbg(addr string) (*FTP, error) {
-	var err error
-	var conn net.Conn
-
-	if conn, err = net.Dial("tcp", addr); err != nil {
+	conn, err := net.Dial("tcp", addr)
+	if err != nil {
 		return nil, err
 	}
+	return UseConnectionDbg(addr, conn)
+}
 
+// Use existing connection to build ftp object, debug is OFF
+func UseConnection(addr string, conn net.Conn) (*FTP, error) {
 	writer := bufio.NewWriter(conn)
 	reader := bufio.NewReader(conn)
+	object := &FTP{conn: conn, addr: addr, reader: reader, writer: writer, debug: false}
+	object.receive()
+	return object, nil
+}
 
-	var line string
-
+// Use existing connection to build ftp object, debug is ON
+func UseConnectionDbg(addr string, conn net.Conn) (*FTP, error) {
+	writer := bufio.NewWriter(conn)
+	reader := bufio.NewReader(conn)
 	object := &FTP{conn: conn, addr: addr, reader: reader, writer: writer, debug: true}
-	line, _ = object.receive()
-
+	line, _ := object.receive()
 	log.Print(line)
-
 	return object, nil
 }
 
